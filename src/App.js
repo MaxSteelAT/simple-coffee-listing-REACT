5e22e7843beb1ef905fdaf220ef560e5a3fe1e5d
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-06-04 11:30:05 -0700

    Se agregó el estilo y la lógica al proyecto.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [coffeeList, setCoffeeList] = useState([]);
+  const [allProducts, setAllProducts] = useState(true);
+  const [available, setAvailable] = useState(false);
+
+  const fetchCoffee = async () => {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/4-frontend-libaries/challenges/group_1/data/simple-coffee-listing-data.json"
+      );
+      const data = await response.json();
+      setCoffeeList(data);
+    } catch (error) {
+      console.log("Hola, si hay error");
+    }
+  };
+
+  useEffect(() => {
+    fetchCoffee();
+  }, []);
+
+  let filteredCoffeeList = coffeeList;
+
+  if (available && !allProducts) {
+    filteredCoffeeList = filteredCoffeeList.filter((item) => item.available);
+  }
+
+  const handleAllProducts = () => {
+    setAllProducts(true);
+    setAvailable(false);
+  };
+
+  const handleAvailable = () => {
+    setAllProducts(false);
+    setAvailable(true);
+  };
+
   return (
     <div className="App">
       <div className="background-header"></div>
@@ -14,20 +51,46 @@ function App() {
           </p>
         </div>
         <div className="filter-container">
-          <button>All Products</button>
-          <button> Available Now</button>
+          <button
+            onClick={handleAllProducts}
+            className={allProducts ? "active" : ""}
+          >
+            All Products
+          </button>
+          <button
+            onClick={handleAvailable}
+            className={available ? "active" : ""}
+          >
+            {" "}
+            Available Now{" "}
+          </button>
         </div>
-        <div className="coffee-listing-container">
-          {/* <img alt="">img</img> */}
-          <div className="coffe-price">
-            <h3>coffee</h3>
-            <h3>$5.20</h3>
-          </div>
-          <div className="qualification state">
-            {/* <img alt="">icon</div> */}
-            <h3>4.7</h3>
-            <h3>Sold out</h3>
-          </div>
+        <div className="coffee-grid">
+          {filteredCoffeeList.map((item, index) => (
+            <div key={index} className="coffee-listing-container">
+              <div className="coffee-image">
+                <div className="popular-icon">
+                  {item.popular && (
+                    <div className="popular-container">
+                      <a className="popular">Popular</a>
+                    </div>
+                  )}
+                </div>
+                <img src={item.image} alt={item.name} />
+              </div>
+              <div className="coffe-price">
+                <h3>{item.name}</h3>
+                <h3 className="price">{item.price}</h3>
+              </div>
+              <div className="qualification">
+                <h3>
+                  ⭐ {item.rating}
+                  <a>({item.votes})</a>
+                </h3>
+                <h3 className="state">{item.available ? "" : "Sold Out"}</h3>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
